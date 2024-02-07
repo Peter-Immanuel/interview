375848dffdb30314aa4fb2111f675c8a069f0fe7
Peter-Immanuel <peterbemshima@gmail.com>
2024-02-07 14:49:03 +0100

    Fix: removed state

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,43 @@
 import Question from "./questions";
 import useState from "react"
 
-export default function Home() {
+function Home() {
 
   
   const questions = [
     {
-      question: "Sample Question",
-      options:["A", "B", "C"],
-      correct: "B",
-      }
-  ]
+      question: "What is the capital of FCT",
+      options:["Abuja", "Makurdi", "Jos"],
+      correct: "Abuja",
+    },
+    {
+      question: "What is the capital of Benue",
+      options:["Abuja", "Makurdi", "Jos"],
+      correct: "Makurdi",
+    },
+    {
+      question: "What is the capital of Jos",
+      options:["Abuja", "Makurdi", "Jos"],
+      correct: "Jos",
+    },
+    {
+      question: "What is the capital of Niger",
+      options:["Abuja", "Makurdi", "Minna"],
+      correct: "Abuja",
+    },
+  ];
 
-  const [count, setCount] = useState(1);
+  // const [count, setCount] = useState(1)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className=""> 
         <div className="mb-4">Question Quiz</div>
-          <Question question={count}/>
-          // Button should be hear
+          <Question question={questions[0]}/>
       </div>
     </main>
   );
 }
+
+
+export default Home
